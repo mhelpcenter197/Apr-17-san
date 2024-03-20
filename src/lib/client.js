515aeb5b6c7fdcbac1b9de3eed7161302208dd28
canurdikfit <161978264+canurdikfit@users.noreply.github.com>
2024-03-20 22:02:51 +0100

    Update client.js

--- src/lib/client.js
+++ src/lib/client.js
@@ -1,7 +1,7 @@
 import SanityClient from "@sanity/client";
 
 export const client = SanityClient({
-  projectId: "7cnft9b3",
+  projectId: "22wmublb",
   dataset: "production",
   apiVersion: "2024-01-10",
   useCdn: true,
